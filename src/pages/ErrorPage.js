80d0e4998bd6d6699b519cb0dad62f78f3837f58
HongYang Gao <hongyang.gao@gmail.com>
2019-08-20 21:53:15 -0700

    Create Banner component

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
-
+import { Link } from 'react-router-dom';
+import Banner from '../components/Banner';
 
 export default function Error() {
     return (
-        <Hero ></Hero>
+        <Hero >
+            <Banner title='404' subtitle='page not found'>
+                <Link to='/' className='btn-primary'>Return Home</Link>
+            </Banner>
+        </Hero>
     )
 }
