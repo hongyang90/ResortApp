69c36221a90a4605447e15743c500a97a4671f2d
HongYang Gao <hongyang.gao@gmail.com>
2019-08-20 21:15:24 -0700

    Add Hero Component to pages

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+
 
 export default function Rooms() {
     return (
-        <div>
-            Hi rooms page
-        </div>
+        <Hero hero='roomsHero'></Hero>
     )
 }
