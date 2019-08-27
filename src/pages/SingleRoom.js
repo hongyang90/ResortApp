052b85d6dc9cec2465c153bc6b01563d5e002df4
HongYang Gao <hongyang.gao@gmail.com>
2019-08-27 16:30:38 -0700

    Add images and info to singlePage Component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -33,14 +33,42 @@ export default class SingleRoom extends Component {
 
         const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
 
+        const [mainImg, ...defaultImg] = images;
         return (
-            <StyledHero img={images[0]} hero='roomsHero'>
-                <Banner title={`${name} room`}>
-                    <Link to='/rooms' className='btn-primary' >
-                        Back to rooms
-                    </Link>
-                </Banner> 
-            </StyledHero>
+            <>
+                <StyledHero img={mainImg} hero='roomsHero'>
+                    <Banner title={`${name} room`}>
+                        <Link to='/rooms' className='btn-primary' >
+                            Back to rooms
+                        </Link>
+                    </Banner> 
+                </StyledHero>
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {defaultImg.map((item, index)=>{
+                           return <img key={index} src={item} alt={name}/>
+                        } )}
+                    </div>
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price : $ {price}</h6>
+                            <h6>size: {size} sq ft</h6>
+                            <h6>
+                                max capacity: {
+                                    capacity > 1 ? `${capacity} people` : `${capacity} person`
+                                }
+                            </h6>
+                            <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+                            <h6>{breakfast && 'free breakfast included'}</h6>
+                        </article>
+                    </div>
+                </section>
+            </>
         )
     }
 }
