2bfd8391eb2ca4b1f0e0405791923a40ac60caea
HongYang Gao <hongyang.gao@gmail.com>
2019-08-26 20:32:09 -0700

    Setup state and basic skeleton for render method in SIngle Room component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../Context';
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+        // console.log(this.props)
+    }
+
+    static contextType = RoomContext;
+
     render() {
-        return (
-            <div>
-                Single Room 
+
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if (!room) {
+            return (
+            <div className="error">
+                <h3>no room found</h3>
+                <Link to='/rooms' className='btn-primary' >Back to room</Link>
             </div>
+            )
+        }
+
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
+
+        return (
+            <Hero hero='roomsHero'>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary' >
+                        Back to rooms
+                    </Link>
+                </Banner> 
+            </Hero>
         )
     }
 }
