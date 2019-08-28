a514bded9bc644a3bd9ef0f4547a7942e4e86bdd
HongYang Gao <hongyang.gao@gmail.com>
2019-08-27 22:19:55 -0700

    Add variables and #getUnique to RoomFilter component

--- src/components/RoomsFilter.jsx
+++ src/components/RoomsFilter.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import {useContext} from 'react';
+import {RoomContext} from '../Context';
+import Title from '../components/Title';
 
-export default function RoomsFilter() {
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+export default function RoomsFilter({rooms}) {
+
+    const context = useContext(RoomContext);
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context;
+
     return (
-        <div>
-            rooms filter
-        </div>
+        <section className='filter-container'> 
+            <Title title='search rooms' />
+            <form className='filter-form'>
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select name="type" id="type" value={type} 
+                    className='form-control' 
+                    onChange={handleChange}>
+
+                    </select>
+                </div>
+            </form>
+        </section> 
     )
 }
